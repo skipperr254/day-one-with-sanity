d03402a806390feef0596d9870da5ba104a8b716
skipperr254 <skipperr254@gmail.com>
2024-07-18 11:13:21 +0300

    added event, artist and venue schema types

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,9 @@
-export const schemaTypes = []
+import { eventType } from "./eventType";
+import { venueType } from "./venueType";
+import { artistType } from "./artistType";
+
+export const schemaTypes = [
+    eventType,
+    venueType,
+    artistType
+]
